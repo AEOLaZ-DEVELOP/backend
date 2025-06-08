72b42809dc3ea6e4ad87166803600e2c5266e23d
AEOLaZ-DEVELOP <AEOLaZ.Developer@gmail.com>
2025-06-08 21:48:40 +0700

    route register

---
+++ src/routes/user.routes.js
@@ -0,0 +1,6 @@
+import {Route} from 'express'
+import { registerUser } from '../controllers/user.controller'
+
+const router= Route()
+router.route('register').post(registerUser)
+export default router

bb168bf921fb3fdf9285f4048c0b5290b693f56e
AEOLaZ-DEVELOP <AEOLaZ.Developer@gmail.com>
2025-06-08 22:07:01 +0700

    route update register

--- src/controllers/user.controller.js
+++
@@ -1,7 +0,0 @@
-import {asyncHandler} from '../utils/asyncHandler'
-
-const registerUser= asyncHandler(async(req,res)=>{
-    return res.status(200).json({message: 'OK'})
-})
-
-export {registerUser}
